e5f368cde31cee2914d6df90a5e61b37b3ab0ba6
Gaby Wald <30340425+gabywald@users.noreply.github.com>
2024-08-13 21:24:31 +0200

    [DiversTestsJava]Colored Noises : some notes / review to be done !

--- src/main/java/gabywald/sound/colorednoises/ColoredNoiseViolet.java
+++ src/main/java/gabywald/sound/colorednoises/ColoredNoiseViolet.java
@@ -8,8 +8,27 @@ class ColoredNoiseViolet extends AbstractSamplingNoise {
 
 	@Override
 	public void run() {
-		// TODO Auto-generated method stub
+		
+		// TODO review here (opposite of Brown / Red Noise ?)
+		
+		if ( ! this.init()) { return; }
 
+        double lastOut = 0.0;
+
+        while(super.isActiv()) {
+            super.buffer.clear();
+
+            for (int i = 0 ; i < super.BASE_CONFIG.PACKET_SIZE / super.BASE_CONFIG.SAMPLE_SIZE ; i++) {
+                double white = super.random.nextGaussian();
+                double output = (lastOut - (0.02 * white)) / 1.02;
+                lastOut = output;
+                output *= 1.5; // (roughly) compensate for gain
+                super.buffer.putShort((short)(output * Short.MAX_VALUE));
+            }
+
+            super.line.write(super.buffer.array(), 0, super.buffer.position());
+        }
+		
 	}
 
 }
